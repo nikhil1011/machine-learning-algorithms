19236f67f2279d616e4d2d8005949cbd124563db
Nikhil Vandanapu <nikhilvan10@gmail.com>
2018-02-27 18:51:03 -0600

    train method partially complete

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -12,15 +12,46 @@ public class Perceptron {
 		}
 	}
 	
-	public List<Double> train(List<List<Double>> inputs){
+	public List<Double> train(Map<List<Double>,Integer> inputs){
 		if(inputs == null) {
 			return null;
 		}
-		if(inputs.get(0).size()!=weights.size() - 1) {
+		
+		Map.Entry<List<Double>, Integer> entry = inputs.entrySet().iterator().next();
+		if(entry.getKey().size() != weights.size() - 1) {
 			return null;
+		}
+//		if(inputs.get(0).size()!=weights.size() - 1) {
+//			return null;
+//		}
+		
+		boolean misclassified = true;
+		while(misclassified) {
+			misclassified = false;
+			for(List<Double> dataPoint: inputs.keySet()) {
+				Integer outputClass = testDataPoint(dataPoint);
+			}
 		}
 		
 		return weights;
+	}
+	
+	public Integer testDataPoint(List<Double> dataPoint) {
+		int outputClass;
+		
+		double result = weights.get(0);
+		for(int i = 0; i<dataPoint.size(); i++) {
+			result = result + dataPoint.get(i)*weights.get(i+1);
+		}
+		
+		if(result>=0) {
+			outputClass = 1;
+		}
+		else {
+			outputClass = -1;
+		}
+		
+		return outputClass;
 	}
 	
 	public static void main(String[] args) {
